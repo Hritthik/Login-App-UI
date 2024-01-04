659c4a99c5a3ea3c120c46fa28f15569362348f8
Nikolas Rodriguez <nikolasRI780mail.com>
2024-01-04 01:29:11 -0300

    full app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+// REACT
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+
+  return (
+
+    <SafeAreaView>
+
+      <Text>Home Screen</Text>
+
+    </SafeAreaView>
+
+  );
+}
